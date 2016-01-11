6da9923f2601a8d1cfb1ce259f584a7247fdd117
unknown <rmeda@BLRRMEDA45612.sapient.com>
2016-01-11 18:38:12 +0530

    modified files

--- scripts/example.js
+++ scripts/example.js
@@ -154,6 +154,7 @@ var GoogleMap = React.createClass({
 
     return (
       <div className="googleMap">   
+	  <a target="_blank" href="https://github.com/rkrishn/"><img className="image" src="https://camo.githubusercontent.com/121cd7cbdc3e4855075ea8b558508b91ac463ac2/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f677265656e5f3030373230302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_green_007200.png" /></a>
         <h1 id="heading"></h1>          
         <div className="Box">  
         <ListComponent data={this.state.data} ref="mapInfo"/>             
